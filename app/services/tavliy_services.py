6070cef548c372993896a384fbf98ca0cc9fa16d
kz-ow <okawa.kazaki.21@shizuoka.ac.jp>
2025-05-23 09:56:57 +0900

    updated tavily mode selector

--- app/services/tavliy_services.py
+++ app/services/tavliy_services.py
@@ -6,14 +6,27 @@ client =  AsyncTavilyClient(api_key=settings.TAVILY_API_KEY)
 
 async def search_articles(
   text:str,
+  mode: str = "news",
 ):
-    response = await client.search(
-        query=text,
-        topic="news",
-        days=4,
-        limit=1,
-        include_images = True,
-
-    )
+    if not text and text == "":
+        raise ValueError("text must be provided")
     
+    if mode == "news":
+      response = await client.search(
+          query=text,
+          topic="news",
+          days=4,
+          limit=1,
+          include_images = True,
+      )
+
+    elif mode == "general":
+       response = await client.search(
+          query=text,
+          topic="general",
+          days=4,
+          limit=1,
+          include_images = True,
+      )
+       
     return response.get("results", []), response.get("images", [])
