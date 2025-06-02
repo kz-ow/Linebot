b2fe0a48b4481c2bc797e31b5dfcf5d63c011b9a
kz-ow <okawa.kazaki.21@shizuoka.ac.jp>
2025-06-02 13:18:05 +0900

    updated prod env

--- app/config.py
+++ app/config.py
@@ -1,13 +1,17 @@
-# app/config.py
-from pydantic_settings import BaseSettings
-from typing import List
+from pydantic_settings import SettingsConfigDict
+from pydantic_settings_aws import SecretsManagerBaseSettings
 
-class Settings(BaseSettings):
+class AppSettings(SecretsManagerBaseSettings):
+    model_config = SettingsConfigDict(
+        secrets_name="your/secret/name"  # AWS Secrets Managerのシークレット名
+    )
+
     LINE_CHANNEL_SECRET: str
     LINE_CHANNEL_ACCESS_TOKEN: str
-    POSTGRES_URL: str      
-    POSTGRES_USER: str     
-    POSTGRES_PASSWORD: str 
+    POSTGRES_URL: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
     GEMINI_API_KEY: str
     GEMINI_REGION: str
     LIFF_ID_MODE: str
@@ -15,20 +19,9 @@ class Settings(BaseSettings):
     LIFF_ID_LANGUAGE: str
     LIFF_CHANNEL_ID: str
     TAVILY_API_KEY: str
+    LANGUAGES: list[str] = [
+        "日本語", "英語", "フランス語", "ドイツ語",
+        "スペイン語", "韓国語", "中国語", "ロシア語"
+    ]
 
-    LANGUAGES: List[str] = (
-        "日本語",
-        "英語",
-        "フランス語",
-        "ドイツ語",
-        "スペイン語",
-        "韓国語",
-        "中国語",
-        "ロシア語"
-    )
-
-    class Config:
-        env_file = ".env"
-        from_attributes = True
-
-settings = Settings()
+settings = AppSettings()
